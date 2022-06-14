f25e1b86f6b68ab4cc46dc7f7ff57d6d31deb449
Sankalp Srivastava <sankalpsri110@gmail.com>
2022-06-14 22:28:02 +0530

    first commit'
    git clear

---
+++ func.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('.link')
+const section = document.querySelectorAll('section')
+
+let activelinks = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
